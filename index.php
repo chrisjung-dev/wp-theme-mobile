5a86c4af9942e1da106281794c95e9ea82be12ab
Chris Jung <campino2k@gmail.com>
2011-03-01 23:28:25 +0100

    Add collapsible Categories
    - fix list vs single item display

--- index.php
+++ index.php
@@ -12,41 +12,51 @@
 			<div data-role="header">
 				<h1><?php bloginfo('name'); ?></h1>
 				<?php if( is_single() ) { ?>
-				<a href="<?php get_home_url(); ?>" data-icon="home" data-iconpos="notext" data-direction="reverse" class="ui-btn-right jqm-home">Home</a>
+				<a href="<?php echo get_home_url(); ?>" data-icon="home" data-iconpos="notext" data-direction="reverse" class="ui-btn-right jqm-home">Home</a>
 				<?php }; ?>
 			</div> 
 			<div data-role="content">
 			
 				<?php if (have_posts()) : ?>
 
-				<?php while (have_posts()) : the_post(); ?>
-					
 					<?php if ( !is_single() ) : ?>
+					<ul data-role="listview" data-inset="true">
+					<?php endif; ?>
+
+					<?php while (have_posts()) : the_post(); ?>
 					
-					<ul data-role="listview">
+					
+						<?php if ( !is_single() ) : ?>
 						<li>
 							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 							<p><?php the_excerpt(); ?></p>
+
 						</li>
+					
+						<?php else : ?>
+
+						<h2><?php the_title(); ?></h2>
+					
+						<div>
+						<?php the_content(); ?>
+						</div>
+
+						<?php endif; ?>
+
+					<?php endwhile; ?>
+
+					<?php if ( !is_single() ) : ?>
 					</ul>
-					
-					<?php else : ?>
-
-					<h2><?php the_title(); ?></h2>
-					
-					<div>
-					<?php the_content(); ?>
-					</div>
-
 					<?php endif; ?>
-
-				<?php endwhile; ?>
-
 				<?php else: ?>
 
-				
-
 				<?php endif; ?>
+			
+				<div data-role="collapsible" data-collapsed="true">
+					<h2>Kategorien</h2>
+
+					<?php wp_list_categories( "title_li=" ); ?> 
+				</div>
 
 
 			</div> 
